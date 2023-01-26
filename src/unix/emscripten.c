3781320e8327adf6749e392a9fe3d9bdd91720e8
Raymond Huffman <rhuffman@mit.edu>
2023-01-26 05:41:10 -0500

    try using EM_ASM to query memory info

--- src/unix/emscripten.c
+++ src/unix/emscripten.c
@@ -3,22 +3,25 @@
 
 #include "string.h"
 
+#include "emscripten.h"
+
+
 int uv_exepath(char* buffer, size_t* size) {
-  strcpy(buffer, "julia");
+  strcpy(buffer, "");
   *size = strlen(buffer);
   return 0;
 }
 
 uint64_t uv_get_available_memory(void) {
-  return 10000000;
+  return uv_get_free_memory();
 }
 
 uint64_t uv_get_free_memory(void) {
-  return 10000000;
+  return uv_get_total_memory() - EM_ASM_INT({ return HEAP8.length });
 }
 
 uint64_t uv_get_total_memory(void) {
-  return 10000000;
+  return return EM_ASM_INT({ return MAXIMUM_MEMORY });;
 }
 
 void uv_loadavg(double avg[3]) {
